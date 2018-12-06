c64cebe16ae30f50d0de1d934d65c180004205ae
Jordan <jordanhaddadi@gmail.com>
2018-12-05 23:44:38 -0500

    updated validateFolderIds and validateTagIds based on the provided solution

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,60 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Note = require('../models/note');
+const Folder = require('../models/folder');
+const Tag = require('../models/tag');
+
+function validateFolderId(folderId, userId) {
+
+  if (folderId === undefined){
+    return Promise.resolve();
+  }
+
+  if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
+    const err = new Error('The `folderId` is not valid');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  return Folder.countDocuments({ _id: folderId, userId })
+    .then (count => {
+      if (count === 0) {
+      const err = new Error('The `folderId` is not valid');
+      err.status = 400;
+      return Promise.reject(err);
+    }
+  });
+}
+
+function validateTagIds(tags, userId) {
+
+  if (tags === undefined){
+    return Promise.resolve();
+  }
+
+  if (!Array.isArray(tags)) {
+    const err = new Error('The `tags` property must be an array');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  const badIds = tags.filter((tag) => !mongoose.Types.ObjectId.isValid(tag));
+  if (badIds.length) {
+    const err = new Error('The `tags` array contains an invalid `id`');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+    
+
+  return Tag.find({ $and: [{ _id: { $in: tags }, userId }] })
+    .then (results => {
+      if (tags.length === results.length) {
+      const err = new Error('The `tag array` contains an invalid id');
+      err.status = 400;
+      return Promise.reject(err);
+    }
+  });
+} 
 
 const router = express.Router();
 
@@ -76,34 +130,22 @@ router.post('/', (req, res, next) => {
   const { title, content, folderId, tags } = req.body;
   const userId = req.user.id;
 
-  /***** Never trust users - validate input *****/
+  /***** Never trust users - validate input *****/  
   if (!title) {
     const err = new Error('Missing `title` in request body');
     err.status = 400;
     return next(err);
   }
 
-  if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
-    const err = new Error('The `folderId` is not valid');
-    err.status = 400;
-    return next(err);
-  }
-
-  if (tags) {
-    const badIds = tags.filter((tag) => !mongoose.Types.ObjectId.isValid(tag));
-    if (badIds.length) {
-      const err = new Error('The `tags` array contains an invalid `id`');
-      err.status = 400;
-      return next(err);
-    }
-  }
-
   const newNote = { title, content, folderId, tags, userId };
   if (newNote.folderId === '') {
     delete newNote.folderId;
   }
-
-  Note.create(newNote)
+  Promise.all([
+    validateFolderId(newNote.folderId, userId),
+    validateTagIds(newNote.tags, userId)
+  ])
+    .then(() => Note.create(newNote))
     .then(result => {
       res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
